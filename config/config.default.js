3de22d9f301232440fa05ee0650227e54cd29675
hongding0211 <keith.dh@hotmail.com>
2023-02-15 23:11:19 +0800

    chore: prettier & eslint

--- config/config.default.js
+++ config/config.default.js
@@ -1,30 +1,30 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+'use strict'
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {})
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1676389048099_6333';
+  config.keys = appInfo.name + '_1676389048099_6333'
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = []
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
-  };
+  }
 
   return {
     ...config,
     ...userConfig,
-  };
-};
+  }
+}
