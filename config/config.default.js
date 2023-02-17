6bcbe6659fec00b1e59690e793cafe9823ca7613
hongding0211 <keith.dh@hotmail.com>
2023-02-17 22:21:25 +0800

    feat: traffic middleware

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,7 @@ module.exports = (appInfo) => {
   config.keys = appInfo.name + '_1676389048099_6333'
 
   // add your middleware config here
-  config.middleware = ['auth']
+  config.middleware = ['auth', 'traffic']
 
   // add your user config here
   const userConfig = {
@@ -31,6 +31,11 @@ module.exports = (appInfo) => {
       path: `${ssoBaseUrl}/api/userInfo`,
       method: 'GET',
     },
+  }
+
+  config.traffic = {
+    windowSize: 60 * 1000,
+    maxRequest: 100,
   }
 
   return {
